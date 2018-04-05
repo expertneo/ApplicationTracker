e2dbe69cae083e475a85f196117796f15efbedd5
Bence Papp <bence.papp.hu@gmail.com>
2018-04-05 13:44:16 +0200

    initializing the base application

---
+++ src/main/java/com/personalproject/applicationtracker/ApplicathionTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.personalproject.applicationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicathionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicathionTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/personalproject/applicationtracker/ApplicathionTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.personalproject.applicationtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicathionTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
